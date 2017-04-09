a7062c2b5bb30b7696e560ce0b615e81b110bcb3
Ingwersaft <Ingwersaft@users.noreply.github.com>
2017-04-09 16:56:29 +0200

    simplified psiElement generation greatly: no more problems with the generated class fields - todo: polish

--- src/com/kesselring/valuegenerator/GenerateValueClass.java
+++ src/com/kesselring/valuegenerator/GenerateValueClass.java
@@ -13,7 +13,6 @@ import com.intellij.psi.codeStyle.CodeStyleManager;
 import com.intellij.psi.impl.source.PsiClassImpl;
 import com.intellij.psi.util.PsiUtilBase;
 import com.kesselring.valuegenerator.generator.CreateValueClass;
-import com.kesselring.valuegenerator.generator.CreateValueClassFields;
 import com.kesselring.valuegenerator.parsed.SourceClass;
 import com.kesselring.valuegenerator.parsed.Type;
 import com.kesselring.valuegenerator.parsed.Variable;
@@ -60,6 +59,21 @@ public class GenerateValueClass extends EditorAction {
                         .collect(Collectors.toList());
 
                 PsiClassImpl psiJavaClass = getRootClassUnderOperation(psiFile);
+
+                PsiClass classFromText =
+                        JavaPsiFacade.getInstance(project).getElementFactory().createClassFromText(
+                                new CreateValueClass(variables, sourceClass, project).asString(), null);
+                classFromText.setName(sourceClass.getName());
+                System.out.println("classFromText.getInnerClasses####");
+                System.out.println(classFromText.getText() + "\n\n\n");
+                System.out.println("####");
+                Stream.of(classFromText.getAllFields()).forEach(psiField -> System.out.println(psiField.getText()));
+                System.out.println("#");
+                Stream.of(classFromText.getConstructors()).forEach(psiMethod -> System.out.println(psiMethod.getText()));
+                System.out.println("#");
+                Stream.of(classFromText.getAllInnerClasses()).forEach(psiClass -> System.out.println(psiClass.getText()));
+                System.out.println("####");
+
                 if (psiJavaClass == null) return;
                 Runnable runnable = () -> {
                     // deleting old fields
@@ -77,22 +91,28 @@ public class GenerateValueClass extends EditorAction {
 
                     System.out.println("start of additions:");
 
+                    Stream.of(classFromText.getAllFields())
+                            .forEach(psiField -> psiJavaClass.add(psiField));
+                    Stream.of(classFromText.getConstructors())
+                            .forEach(psiMethod -> psiJavaClass.add(psiMethod));
+                    Stream.of(classFromText.getAllInnerClasses())
+                            .forEach(psiClass -> psiJavaClass.add(psiClass));
                     // adding constructor und value subclasses
-                    new CreateValueClass(variables, sourceClass, project).asPsi()
-                            .stream()
-                            .peek(psiElement -> System.out.println("going to add:\n" + psiElement.getText()))
-                            .forEach(psiElement ->
-                                    psiJavaClass.add(psiElement));
-
-                    System.out.println("after createvalueclass additions:\n" + psiJavaClass.getText());
-                    // adding fields
-                    PsiMethod constructor = psiJavaClass.getConstructors()[0];
-                    List<PsiElement> valueClassFields = new CreateValueClassFields(variables, project).asPsi();
-                    valueClassFields
-                            .forEach(psiElement -> {
-                                psiJavaClass.addBefore(psiElement, constructor);
-                            });
-                    System.out.println("after field additions:\n" + psiJavaClass.getText());
+//                    new CreateValueClass(variables, sourceClass, project).asPsi()
+//                            .stream()
+//                            .peek(psiElement -> System.out.println("going to add:\n" + psiElement.getText()))
+//                            .forEach(psiElement ->
+//                                    psiJavaClass.add(psiElement));
+//
+//                    System.out.println("after createvalueclass additions:\n" + psiJavaClass.getText());
+//                    // adding fields
+//                    PsiMethod constructor = psiJavaClass.getConstructors()[0];
+//                    List<PsiElement> valueClassFields = new CreateValueClassFields(variables, project).asPsi();
+//                    valueClassFields
+//                            .forEach(psiElement -> {
+//                                psiJavaClass.addBefore(psiElement, constructor);
+//                            });
+//                    System.out.println("after field additions:\n" + psiJavaClass.getText());
 
                     CodeStyleManager.getInstance(project).reformat(psiFile);
                 };
@@ -115,5 +135,75 @@ public class GenerateValueClass extends EditorAction {
         }
         PsiClassImpl psiJavaClass = javaClass.get();
         return psiJavaClass;
+    }
+
+    public static String getTestClass() {
+        return "public class Person {\n" +
+                "    private Name name;\n" +
+                "    private Surname surname;\n" +
+                "    private Age age;\n" +
+                "    private Alive alive;\n" +
+                "    private SystemColor nonPrimitiveOrPrimitiveWrapper;\n" +
+                "\n" +
+                "    public Person(Name name, Surname surname, Age age, Alive alive, SystemColor nonPrimitiveOrPrimitiveWrapper) {\n" +
+                "        this.name = name;\n" +
+                "        this.surname = surname;\n" +
+                "        this.age = age;\n" +
+                "        this.alive = alive;\n" +
+                "        this.nonPrimitiveOrPrimitiveWrapper = nonPrimitiveOrPrimitiveWrapper;\n" +
+                "    }\n" +
+                "\n" +
+                "    public static final class Name {\n" +
+                "        private final String name;\n" +
+                "\n" +
+                "        public String get() {\n" +
+                "            return name;\n" +
+                "        }\n" +
+                "\n" +
+                "        public Name(String name) {\n" +
+                "            this.name = name;\n" +
+                "        }\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "    public static final class Surname {\n" +
+                "        private final String surname;\n" +
+                "\n" +
+                "        public String get() {\n" +
+                "            return surname;\n" +
+                "        }\n" +
+                "\n" +
+                "        public Surname(String surname) {\n" +
+                "            this.surname = surname;\n" +
+                "        }\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "    public static final class Age {\n" +
+                "        private final Integer age;\n" +
+                "\n" +
+                "        public Integer get() {\n" +
+                "            return age;\n" +
+                "        }\n" +
+                "\n" +
+                "        public Age(Integer age) {\n" +
+                "            this.age = age;\n" +
+                "        }\n" +
+                "\n" +
+                "    }\n" +
+                "\n" +
+                "    public static final class Alive {\n" +
+                "        private final Boolean alive;\n" +
+                "\n" +
+                "        public Boolean get() {\n" +
+                "            return alive;\n" +
+                "        }\n" +
+                "\n" +
+                "        public Alive(Boolean alive) {\n" +
+                "            this.alive = alive;\n" +
+                "        }\n" +
+                "\n" +
+                "    }\n" +
+                "}";
     }
 }
