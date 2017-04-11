8e9501cd3cbedd4a3fef3a0e72855c035103c66c
Ingwersaft <Ingwersaft@users.noreply.github.com>
2017-04-11 21:24:26 +0200

    added static of method generation to subclasses resolving #5

--- src/com/kesselring/valuegenerator/generator/ValueSubClass.java
+++ src/com/kesselring/valuegenerator/generator/ValueSubClass.java
@@ -1,21 +1,30 @@
 package com.kesselring.valuegenerator.generator;
 
+import com.kesselring.valuegenerator.parsed.Type;
 import com.kesselring.valuegenerator.parsed.Variable;
 
 import java.util.StringJoiner;
 
 public class ValueSubClass {
     private Variable variable;
+    private String className;
+    private String nameValue;
+    private String classType;
 
     public ValueSubClass(Variable variable) {
         this.variable = variable;
+        this.classType = variable.getType().getClassName().getValue();
+        this.nameValue = variable.getName().getValue();
+        this.className = nameValue.substring(0, 1).toUpperCase() + nameValue.substring(1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValueSubClass(
+                new Variable(new Type("java.lang.String"), new Variable.Name("testValue"))).asString());
     }
 
     public String asString() {
         StringJoiner resultLineJointer = new StringJoiner("\n");
-        String classType = variable.getType().getClassName().getValue();
-        String nameValue = variable.getName().getValue();
-        String className = nameValue.substring(0, 1).toUpperCase() + nameValue.substring(1);
         resultLineJointer.add("public static final class " + className + " {");
         resultLineJointer.add("\tprivate final " + classType + " " + nameValue + ";");
         resultLineJointer.add("\tpublic " + classType + " get() {");
@@ -27,11 +36,14 @@ public class ValueSubClass {
         resultLineJointer.add(createOfMethode());
         resultLineJointer.add("}");
         String s = resultLineJointer.toString();
-        System.out.println("ValueSubClass: " + s);
         return s;
     }
 
     private String createOfMethode() {
-        return "";
+        String result =
+                "\tpublic static " + this.className + " of(final " + classType + " " + nameValue + "){\n" +
+                        "\t\treturn new " + this.className + "(" + nameValue + ");\n" +
+                        "\t}\n";
+        return result;
     }
 }
