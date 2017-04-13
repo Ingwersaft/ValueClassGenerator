59a0b4dc9db398694d94d9dffeb9f19491896d4f
Ingwersaft <Ingwersaft@users.noreply.github.com>
2017-04-13 23:17:18 +0200

    added equals and hashcode to value subclasses #6

--- src/com/kesselring/valuegenerator/generator/ValueSubClass.java
+++ src/com/kesselring/valuegenerator/generator/ValueSubClass.java
@@ -3,6 +3,7 @@ package com.kesselring.valuegenerator.generator;
 import com.kesselring.valuegenerator.parsed.Type;
 import com.kesselring.valuegenerator.parsed.Variable;
 
+import java.util.Objects;
 import java.util.StringJoiner;
 
 public class ValueSubClass {
@@ -20,7 +21,8 @@ public class ValueSubClass {
 
     public static void main(String[] args) {
         System.out.println(new ValueSubClass(
-                new Variable(new Type("java.lang.String"), new Variable.Name("testValue"))).asString());
+                new Variable(new Type("java.lang.String"), new Variable.Name("testValue"))).createEqualsAndHash());
+        System.out.println(Alive.of(Boolean.TRUE).equals(Alive.of(Boolean.TRUE)));
     }
 
     public String asString() {
@@ -34,6 +36,7 @@ public class ValueSubClass {
         resultLineJointer.add("\t\tthis." + nameValue + " = " + nameValue + ";");
         resultLineJointer.add("\t}");
         resultLineJointer.add(createOfMethode());
+        resultLineJointer.add(createEqualsAndHash());
         resultLineJointer.add("}");
         String s = resultLineJointer.toString();
         return s;
@@ -45,5 +48,49 @@ public class ValueSubClass {
                         "\t\treturn new " + this.className + "(" + nameValue + ");\n" +
                         "\t}\n";
         return result;
+    }
+
+    private String createEqualsAndHash() {
+        return "        @Override\n" +
+                "        public boolean equals(Object o) {\n" +
+                "            if (this == o) return true;\n" +
+                "            if (o == null || getClass() != o.getClass()) return false;\n" +
+                "            " + this.className + " " + nameValue + "1 = (" + this.className + ") o;\n" +
+                "            return Objects.equals(" + nameValue + ", " + nameValue + "1." + nameValue + ");\n" +
+                "        }\n" +
+                "\n" +
+                "        @Override\n" +
+                "        public int hashCode() {\n" +
+                "            return Objects.hash(" + nameValue + ");\n" +
+                "        }";
+    }
+
+    public static final class Alive {
+        private final Boolean alive;
+
+        public Alive(Boolean alive) {
+            this.alive = alive;
+        }
+
+        public static Alive of(final Boolean alive) {
+            return new Alive(alive);
+        }
+
+        public Boolean get() {
+            return alive;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Alive alive1 = (Alive) o;
+            return Objects.equals(alive, alive1.alive);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(alive);
+        }
     }
 }
